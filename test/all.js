eb819eab1e689127420fd7e9e404229b13f8c7fc
Robert Thurnher <r.thurnher@gmail.com>
2010-03-26 00:48:16 +0100

    Refine test a bit.

--- test/all.js
+++ test/all.js
@@ -1,15 +1,17 @@
 var mail = require('ringo/mail');
 
-const ADDRESS1 = 'hannes@helma.at';
-const ADDRESS2 = 'ringojs@mailinator.com';
+const ADDRESS_1 = 'hannes@helma.at';
+const ADDRESS_2 = 'ringojs@mailinator.com';
+const ADDRESS_3 = 'commonjs@mailinator.com';
 const SUBJECT = '[Foo] Bar';
 const TEXT = 'Hi!\n\nThis is some text.\n\nCheers, Tester';
 
-exports.testSendMail = function () {
-    mail.send({from: ADDRESS1, to: ADDRESS2, subject: SUBJECT, text: TEXT});
-    mail.send({from: ADDRESS1, to: ADDRESS2, text: TEXT});
-    mail.send({from: ADDRESS1, to: ADDRESS2, subject: SUBJECT});
-    mail.send({from: ADDRESS1, to: ADDRESS2}); // Testing various input options.
+exports.testSendingMailWithVariousInputOptions = function () {
+    mail.send({from: ADDRESS_1, to: ADDRESS_2, subject: SUBJECT, text: TEXT});
+    mail.send({from: ADDRESS_1, to: ADDRESS_2, text: TEXT});
+    mail.send({from: ADDRESS_1, to: ADDRESS_2, subject: SUBJECT});
+    mail.send({from: ADDRESS_1, to: [ADDRESS_2, ADDRESS_3]});
+    mail.send({from: ADDRESS_1, to: ADDRESS_2});
 };
 
 if (require.main == module.id) {
